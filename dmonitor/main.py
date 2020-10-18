a77309b8936f666a33170e96745656ab8319bd04
uburuntu <bekbulatov.ramzan@ya.ru>
2020-10-18 22:07:55 +0300

    new: gui update

--- dmonitor/main.py
+++ dmonitor/main.py
@@ -9,26 +9,47 @@ from stathat import StatHat
 from utils import Timer, CheckMyIP
 
 
+def popup(text: str, blocking: bool = False, copiable: bool = False):
+    text = sg.MultilineOutput(text, size=(500, 300)) if copiable else sg.Text(text)
+    button = sg.Button if blocking else sg.DummyButton
+
+    window = sg.Window(
+        'DMonitor',
+        [[text], [button('OK')]],
+        element_justification='right', resizable=False, icon=config.icon
+    )
+
+    if blocking:
+        event, values = window.read()
+        window.Close()
+        return event
+
+    event, values = window.read(timeout=0)
+    return event, values
+
+
 def main():
+    sg.theme('DarkAmber')
+
     timer = Timer(1 * 60.).start()
     stathat = StatHat(config.stathat_key)
     pinger = Pinger(stathat)
     ip_checker = CheckMyIP()
 
     if ip_checker.get_ip() is None:
-        return sg.popup_ok('Первый запуск программы должен быть при работающем интернете', icon=config.icon)
+        return popup('Первый запуск программы должен быть при работающем интернете', blocking=True)
 
-    timer_notification_1 = Timer(30 * 60.)
-    timer_notification_2 = Timer(30 * 60.)
+    timer_notification_1 = Timer(60 * 60.)
+    timer_notification_2 = Timer(60 * 60.)
 
     menu = ['UNUSED', ['Информация', '---', 'Закрыть']]
     last_send = 'Данные еще не отправлялись'
     tooltip = 'Мониторинг интернета в общежитиях МГУ'
-    tray = sg.SystemTray(menu=menu, tooltip=f'{tooltip}\n\n{last_send}', data_base64=config.icon)
+    tray = sg.SystemTray(menu=menu, tooltip=f'{tooltip}\n\n{last_send}.', data_base64=config.icon)
 
-    sg.popup_no_wait('Мониторинг интернета запущен, выгрузка статистики начнётся через 1 минуту.\n\n'
-                     'Это окно можно закрыть, я продолжу работать в фоновом режиме.\n\n'
-                     'Спасибо за участие!', icon=config.icon)
+    popup('Мониторинг интернета запущен, выгрузка статистики начнётся через 1 минуту.\n\n'
+          'Это окно можно закрыть, я продолжу работать в фоновом режиме.\n\n'
+          'Спасибо за участие!')
 
     try:
         while True:
@@ -39,22 +60,23 @@ def main():
                     ok = pinger.analyze()
                     if ok:
                         last_send = f'Последняя отправка данных: {time.ctime()}'
-                        tray.update(tooltip=f'{tooltip}\n\n{last_send}')
+                        tray.update(tooltip=f'{tooltip}\n\n{last_send}.')
                     else:
                         if timer_notification_1.acquire():
-                            sg.popup_no_wait(f'Проблема с доступом к одному из сайтов: {", ".join(config.domains)}', icon=config.icon)
+                            popup(f'Проблема с доступом к одному из сайтов: {", ".join(config.domains)}.\n\n'
+                                  f'Информация об этом сохранена на диск и будет отправлена при первой возможности.')
                 else:
                     if timer_notification_2.acquire():
-                        sg.popup_no_wait('Кажется, вы подключены не к сети МГУ.\n\n'
-                                         'Выгрузка статистики приостановлена до переподключения к ней.', icon=config.icon)
+                        popup('Кажется, вы подключены не к сети МГУ.\n\nВыгрузка статистики приостановлена до переподключения к ней.')
 
             if event == 'Закрыть':
                 break
 
             if event == 'Информация':
-                sg.popup_no_wait(f'{config.text_about}\n\n{last_send}', icon=config.icon)
+                popup(f'{config.text_about}\n\n{last_send}.', copiable=True)
+
     except Exception as e:
-        sg.popup_ok(f'Exception: {repr(e)}\n\nTraceback: {traceback.format_exc()}', icon=config.icon)
+        popup(f'Exception: {repr(e)}\n\nTraceback: {traceback.format_exc()}', blocking=True)
     finally:
         tray.close()
 
